5b29c4f6b0414eabe4dcb9c1f99f5061d3688cf1
Hércules Costa <dev.hrcules@gmail.com>
2024-04-29 16:08:58 -0300

    feat: add app container

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Forms from "./components/Forms";
 import Header from "./components/Header";
 
+import styles from "./styles/App.module.css";
+
 function App() {
   return (
-    <>
+    <div className={styles.appContainer}>
       <Header />
       <Forms />
-    </>
+    </div>
   );
 }
 
